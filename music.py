0db0b0b5da54d31400569cdf71078586da5a8083
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-12 17:23:35 -0300

    Update music.py

--- music.py
+++ music.py
@@ -12,4 +12,4 @@ async def on_ready():
 async def hello(ctx):
 	await Bot.say("Hello!!!")
 
-Bot.run("NjEwNTYyMjk2NjQ0ODk0NzIz.XVHKCA.sABfX0Nz-NYXDoxQd-Q3OsKRN5A")
+Bot.run("NTcxMzUzNDI3NzI1MjU0NjU3.XVHKtw.bfm5dVTe6_ePSjs0C58QcaCG5Bw")
